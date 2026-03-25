6b8b8eba39865ea464023add2f733b514723dc0d
Lonnie Buser <lrbuser3@gmail.com>
2026-03-25 15:11:19 -0400

    added createJWSjs and modified login api to relfect this change

--- api.js
+++ api.js
@@ -12,26 +12,36 @@ app.post('/api/additem', async (req, res) => {
     }
 });
 
-app.post('/api/login', async (req, res) => {
+
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
     const { login, password } = req.body;
-
-    try {
-        const db = client.db('OutfittrDB');
-        const user = await db.collection('Users').findOne({ Login: login, Password: password });
-
-        if (user) {
-            res.status(200).json({
-                id: user.UserID, // Ensure your DB field is 'UserID' and not '_id'
-                firstName: user.FirstName,
-                lastName: user.LastName,
-                error: ''
-            });
-        } else {
-            res.status(401).json({ error: 'Invalid login/password' });
+    const db = client.db('OutfittrDB');
+    const results = await
+        db.collection('Users').find({ Login: login, Password: password }).toArray
+            ();
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    var ret;
+    if (results.length > 0) {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+        try {
+            const token = require("./createJWT.js");
+            ret = token.createToken(fn, ln, id);
         }
-    } catch (e) {
-        res.status(500).json({ error: e.toString() });
+        catch (e) {
+            ret = { error: e.message };
+        }
     }
+    else {
+        ret = { error: "Login/Password incorrect" };
+    }
+    res.status(200).json(ret);
 });
 
 app.post('/api/searchitems', async (req, res) => {
